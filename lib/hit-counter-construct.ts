1ba32ded8802d8da60eae4adc328f4f779c7101c
pabcrudel <pabcrudel@gmail.com>
2023-07-26 19:49:44 +0200

    Grant the lambda role invoke permissions to the downstream function

--- lib/hit-counter-construct.ts
+++ lib/hit-counter-construct.ts
@@ -32,5 +32,8 @@ export class HitCounter extends Construct {
 
     // grant the lambda role read/write permissions to the table
     table.grantReadWriteData(this.handler);
+
+    // grant the lambda role invoke permissions to the downstream function
+    props.downstream.grantInvoke(this.handler);
   }
 }
