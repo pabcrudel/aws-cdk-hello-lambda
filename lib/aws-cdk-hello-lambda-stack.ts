db687706484a09260e5a2cc705ed5f413ede3db4
pabcrudel <pabcrudel@gmail.com>
2023-07-26 17:27:21 +0200

    Added an aws lambda function to the stack

--- lib/aws-cdk-hello-lambda-stack.ts
+++ lib/aws-cdk-hello-lambda-stack.ts
@@ -1,10 +1,16 @@
 import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 
 export class AwsCdkHelloLambdaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    
+    // Defines an AWS Lambda resource
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_16_X,    // execution environmet
+      code: lambda.Code.fromAsset('lambda'),  // code loaded from "./lambda" directory
+      handler: "helo.handler"                 // file is "hello.js", function is "handler"
+    });
   }
 }
