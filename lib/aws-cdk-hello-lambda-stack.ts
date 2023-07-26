38f91d6d7ffedb53b3b44f2dc50bcbe8b8d506ff
pabcrudel <pabcrudel@gmail.com>
2023-07-26 19:37:59 +0200

    Added hit counter to the hello lambda

--- lib/aws-cdk-hello-lambda-stack.ts
+++ lib/aws-cdk-hello-lambda-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigw from 'aws-cdk-lib/aws-apigateway';
 import { Construct } from 'constructs';
+import { HitCounter } from './hit-counter-construct';
 
 export class AwsCdkHelloLambdaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -14,9 +15,19 @@ export class AwsCdkHelloLambdaStack extends cdk.Stack {
       handler: "hello.handler"                // file is "hello.js", function is "handler"
     });
 
+    // Defines an AWS Lambda resorce that lives between "hello" and API Gateway
+    const helloWithCounter = new HitCounter(this, 'HelloHitCounter', {
+      downstream: hello
+    })
+
     // Defines an API Gateway REST API resource backed by the "hello" function
     new apigw.LambdaRestApi(this, 'Endpoint', {
-      handler: hello
+      handler: helloWithCounter.handler
+      /* 
+        API Gateway will route the request to the hit counter handler, 
+        which will log the hit and relay it over to the hello function. 
+        Then, the responses will be relayed back in the reverse order all the way to the user
+      */
     });
   }
 }
