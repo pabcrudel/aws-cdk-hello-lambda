7129df700bd59d6875e03593ddef87c59325d35d
pabcrudel <pabcrudel@gmail.com>
2023-07-27 11:21:51 +0200

    Test: Read capacity can be configured

--- test/hit-counter-construct.test.ts
+++ test/hit-counter-construct.test.ts
@@ -22,6 +22,21 @@ test('DynamoDB Table Created With Encryption', () => {
     });
 });
 
+test('Read capacity of the DynamoDB Tables can be configured', () => {
+    const stack = new cdk.Stack();
+
+    expect(() => {
+        new HitCounter(stack, 'MyTestConstruct', {
+            downstream: new lambda.Function(stack, 'TestFunction', {
+                runtime: lambda.Runtime.NODEJS_14_X,
+                handler: 'hello.handler',
+                code: lambda.Code.fromAsset('lambda')
+            }),
+            readCapacity: 3
+        });
+    }).toThrowError(/readCapacity must be greater than 5 and less than 20/);
+});
+
 test('Lambda Has Environment Variables', () => {
     const stack = new cdk.Stack();
     // WHEN
